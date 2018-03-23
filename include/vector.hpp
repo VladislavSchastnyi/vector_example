d823b85bcc7ce407135323289168f3c33fdb171a
VladislavSchastnyi <32384821+VladislavSchastnyi@users.noreply.github.com>
2018-03-23 11:48:35 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -4,7 +4,7 @@ class vector_t
 {
 private:
 	int * elements_;
-	std::size_t size_;
+	std::size_t size_;  //  выделение памяти определённого размера
 	std::size_t capacity_;
 public:
 	vector_t();
@@ -18,10 +18,10 @@ public:
 	void push_back(int value);  // перераспределение памяти(увеличение "ячеек" на 2 если size=capacity)
 	void pop_back();  //  удаление элементов(уменьшение "ячеек" в 4 раза если 4*size=capacity)
 
-	int & operator [](std::size_t index);
+	int & operator [](std::size_t index);  //  индексикация(обращение)
 	int operator [](std::size_t index) const;
 
-	bool operator ==(vector_t const & other) const;
+	bool operator ==(vector_t const & other) const;  //  bool-оператор сравнения
 };
 
 bool operator !=(vector_t const & lhs, vector_t const & rhs);
